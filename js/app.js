3207502a65b5ad30c2619d354107899fa37e38d9
April Canete <aprilrose.canete93@gmail.com>
2021-12-03 19:31:51 +1100

    added js feature

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
  var playerOne = {name: 'player 1', choices:[] };
  var playerTwo = {name: 'player 2', choices:[] };
  var gameBoxes = document.querySelectorAll('.game-box');
+ var playAgainBtnWrap = document.querySelector('.play-again-btn-wrapper');
+ var playAgainBtn = document.querySelector('.play-again-btn');
  var winningCombos = [[1,2,3], [1,5,9], [1,4,7], [4,5,6], [7,8,9], [2,5,8], [3,6,9],
             [3,5,7]];
  var counter = 0;
@@ -12,30 +14,15 @@ function addElement(str) {
     var newHeading = document.createElement("h1");
     var newContent = document.createTextNode(str);
 
-    var newDiv = document.createElement('div');
-    var newImg = document.createElement('img');
-    var newTxt = document.createElement('h2');
-    var newTextContent = document.createTextNode(`Play again!`);
-
-    newDiv.className = 'alert-msg-wrapper';
-    newTxt.className = 'play-again-btn-txt';
-
-    newImg.src = 'images/play-again-btn.gif'
-    newImg.className = 'play-again-btn';
-
+    newHeading.className = 'alert-message';
     newHeading.style.textAlign = 'center';
     
-    newTxt.appendChild(newTextContent);
-    newDiv.appendChild(newImg);
-    newDiv.appendChild(newTxt);
     newHeading.appendChild(newContent);
 
     var currentDiv = document.querySelector('.main');
     document.body.insertBefore(newHeading, currentDiv);
-    // document.body.insertBefore(newDiv, currentDiv);
 
-    currentDiv.after(newDiv);
-
+   playAgainBtnWrap.style.display = 'block';
 }
 
 function gameCheck(player, choice) {
@@ -50,27 +37,36 @@ function gameCheck(player, choice) {
             // console.log(isComboWithinUserChoices);
 
             if (isComboWithinUserChoices) {
-               // console.log('this is working!');
+
                 isGameDone = true;
 
                 addElement(`${player} wins!`);
+
+                for (let i = 0; i < gameBoxes.length; i++) {
+                    var box = gameBoxes[i];
+
+                    box.removeEventListener('click', handleClick)
+                }
+                     
             }    
         }
 }
 
 function handleClick(event) {
     var userClicked = event.target;
-    var playerIcon = document.createElement("img");
+    var playerIcon = document.createElement('img');
+
+    playerIcon.className = 'player-icon';
     
     counter++;
-
-    console.log(counter);
+    //console.log(counter);
     //console.log(playerOne);
 
+
     if (counter %2 !== 0) {
-        userClicked.style.backgroundColor = 'purple';
-        // playerIcon.src = "images/bagel-icon.webp";
-        // userClicked.appenChild(playerIcon);
+        
+        playerIcon.src = 'images/coffee-icon.webp';
+        userClicked.appendChild(playerIcon);
 
         playerOne.choices.push(Number(userClicked.dataset.number));
 
@@ -79,27 +75,30 @@ function handleClick(event) {
         gameCheck(playerOne.name, playerOne.choices);
         
     } else {
-        userClicked.style.backgroundColor = 'salmon';
-        // playerIcon.src = "images/coffee-icon.webp";
-        // userClicked.appenChild(playerIcon);
+
+        playerIcon.src = 'images/bagel-icon.webp';
+        userClicked.appendChild(playerIcon);
 
         playerTwo.choices.push(Number(userClicked.dataset.number));
 
         //console.log(playerTwo.name + ' picks: ' + playerTwo.choices);
 
-        gameCheck(playerTwo.name, playerTwo.choices);
-        
+        gameCheck(playerTwo.name, playerTwo.choices);       
     }
 
     if (counter == gameBoxes.length && isGameDone != true) {
         addElement(`It's a draw!`);
     }
+}
 
-   
 
+function handleReset() {
+    window.location.reload();
 }
 
 
 gameBoxes.forEach(box => 
     box.addEventListener('click', handleClick)
     );
+
+playAgainBtn.addEventListener('click', handleReset);
